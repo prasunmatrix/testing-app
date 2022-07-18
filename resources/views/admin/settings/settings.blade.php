5c9b204eaa6d0a75819f8dfafb50657e8994a14d
prasun <prasun@matrixnmedia.com>
2022-07-18 11:02:56 +0530

    setting blade file change

--- resources/views/admin/settings/settings.blade.php
+++ resources/views/admin/settings/settings.blade.php
@@ -12,9 +12,9 @@
     <li class="nav-item" role="presentation">
       <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Social Settings</button>
     </li>
-    <li class="nav-item" role="presentation">
+    <!-- <li class="nav-item" role="presentation">
       <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
-    </li>
+    </li> -->
   </ul>
   <div class="tab-content" id="pills-tabContent">
     <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">  
